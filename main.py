78979d9c25a1b798dbbab0cca1dc94a8b21b87cc
Viswajith J <viswajith.j.official@gmail.com>
2025-03-09 18:05:05 +0530

    Test blueprints

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from app.routes import osint_scrapper
+app = Flask(
+    __name__, template_folder="app/templates", static_folder="app/static"
+)  # templates inside /app/ . so mention explicitly
 
-app = Flask(__name__, template_folder="app/templates") # templates inside /app/ . so mention explicitly
+app.secret_key = "demo_key_hehe"
 
-
+app.register_blueprint(osint_scrapper.osint_scrapper, url_prefix="/osint_scrapper")
+## note to future self: set secret key before registering blueprints (for sessions)
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
-if __name__=="__main__":
-	app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
