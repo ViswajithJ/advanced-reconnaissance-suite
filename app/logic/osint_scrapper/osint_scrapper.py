492b9dbcd1e877d0caadc87d38a54a6a47003713
Viswajith J <viswajith.j.official@gmail.com>
2025-03-13 20:28:46 +0530

    Add osint scrapping

--- app/logic/osint_scrapper/osint_scrapper.py
+++ app/logic/osint_scrapper/osint_scrapper.py
@@ -1,4 +1,19 @@
-from tools import *
+from app.logic.osint_scrapper.tools import *
 
-def osint_scrape(domain):
-	pass
+options_dict = {
+    "Server Status": get_server_status,
+    "IP": get_ip,
+    "Server Location": get_location,
+    "Domain Registration Info": get_whois,
+    "Cookies": get_cookies,
+    "HTTP Headers": get_headers,
+    "SSL Certificate Info": get_ssl_certificate_info,
+    "Firewall Status": get_firewall_info,
+}
+
+
+def osint_scrape(domain, option_list):
+    osint_result = {"Domain": domain}
+    for i in option_list:
+        osint_result[i] = options_dict[i](domain)
+    return osint_result
--- app/routes/osint_scrapper.py
+++ app/routes/osint_scrapper.py
@@ -1,35 +1,38 @@
 from flask import (
-	Blueprint,
-	render_template,
-	jsonify,
-	session,
-	request,
-	redirect,
-	url_for,
+    Blueprint,
+    render_template,
+    jsonify,
+    session,
+    request,
+    redirect,
+    url_for,
 )
 from app.logic.osint_scrapper.osint_scrapper import osint_scrape
+
 osint_scrapper = Blueprint(
-	"osint_scrapper",
-	__name__,
-	template_folder="app/templates",
-	static_folder="app/static",
+    "osint_scrapper",
+    __name__,
+    template_folder="app/templates",
+    static_folder="app/static",
 )  # osint_scrapper: name of blueprint; __name__: import name of blueprint's module
 
 
 @osint_scrapper.route("/input", methods=["GET", "POST"])
 def input():
-	if request.method == "POST":
-		if request.form.get("Submit") == "Submit":
-			domain_name = request.form.get("domain_name")
-			option_list = request.form.getlist("option")
-			session["osint"] = [domain_name, option_list]
-			
-			osint_result = osint_scrape(domain_name)
-			return redirect(url_for("osint_scrapper.output"))
-	return render_template("osint_input.html")
+    if request.method == "POST":
+        if request.form.get("Submit") == "Submit":
+            domain_name = request.form.get("domain_name")
+            option_list = request.form.getlist("option")
+
+            osint_result = osint_scrape(domain_name, option_list)
+            session["osint_option_list"] = option_list
+            session["osint_result"] = osint_result
+
+            return redirect(url_for("osint_scrapper.output"))
+    return render_template("osint_input.html")
 
 
 @osint_scrapper.route("/output", methods=["GET"])
 def output():
-	domain_name = session["domain_name"]
-	return render_template("osint_output.html", domain_name=domain_name)
+    osint_result = session["osint_result"]
+    return render_template("osint_output.html", osint_result=osint_result)
