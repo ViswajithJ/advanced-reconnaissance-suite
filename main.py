fe094ff3fdebf0ff2f16e0b70d630c87a29a9158
Viswajith J <viswajith.j.official@gmail.com>
2025-03-14 00:32:50 +0530

    Add bp for subdom and vulncheck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, session
-from app.routes import osint_scrapper, subdir_enumerator
+from app.routes import osint_scrapper, subdir_enumerator, subdom_enumerator, vuln_checker
 app = Flask(
     __name__, template_folder="app/templates", static_folder="app/static"
 )  # templates inside /app/ . so mention explicitly
@@ -8,6 +8,8 @@ app.secret_key = "demo_key_hehe"
 
 app.register_blueprint(osint_scrapper.osint_scrapper, url_prefix="/osint_scrapper")
 app.register_blueprint(subdir_enumerator.subdir_enumerator, url_prefix="/subdir_enumerator")
+app.register_blueprint(subdom_enumerator.subdom_enumerator, url_prefix="/subdom_enumerator")
+app.register_blueprint(vuln_checker.vuln_checker, url_prefix="/vuln_checker")
 
 ## note to future self: set secret key before registering blueprints (for sessions)
 @app.route("/")
