57fd6f2ade6cc90106cf683eaf8473820d889548
Viswajith J <viswajith.j.official@gmail.com>
2025-03-13 14:34:09 +0530

    Get options form form

--- app/routes/osint_scrapper.py
+++ app/routes/osint_scrapper.py
@@ -1,5 +1,13 @@
-from flask import Blueprint, render_template, jsonify, session, request, redirect, url_for
-
+from flask import (
+	Blueprint,
+	render_template,
+	jsonify,
+	session,
+	request,
+	redirect,
+	url_for,
+)
+from app.logic.osint_scrapper.osint_scrapper import osint_scrape
 osint_scrapper = Blueprint(
 	"osint_scrapper",
 	__name__,
@@ -8,16 +16,20 @@ osint_scrapper = Blueprint(
 )  # osint_scrapper: name of blueprint; __name__: import name of blueprint's module
 
 
-@osint_scrapper.route("/input", methods=['GET', 'POST'])
+@osint_scrapper.route("/input", methods=["GET", "POST"])
 def input():
-	if request.method =='POST':
-		if request.form.get('Submit') == 'Submit':
-			domain_name = request.form.get('domain_name')
-			session['domain_name'] = domain_name 
-			print(domain_name)
-			return redirect(url_for('osint_scrapper.output'))
-	return render_template('osint_input.html')
-@osint_scrapper.route("/output", methods=['GET'])
+	if request.method == "POST":
+		if request.form.get("Submit") == "Submit":
+			domain_name = request.form.get("domain_name")
+			option_list = request.form.getlist("option")
+			session["osint"] = [domain_name, option_list]
+			
+			osint_result = osint_scrape(domain_name)
+			return redirect(url_for("osint_scrapper.output"))
+	return render_template("osint_input.html")
+
+
+@osint_scrapper.route("/output", methods=["GET"])
 def output():
-	domain_name = session['domain_name']
-	return render_template('osint_output.html', domain_name=domain_name)
+	domain_name = session["domain_name"]
+	return render_template("osint_output.html", domain_name=domain_name)
