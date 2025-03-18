cd9a22c285654f5b4e78d20ae5cbacbbaf4a83bd
Viswajith J <viswajith.j.official@gmail.com>
2025-03-18 21:40:08 +0530

    Update logic

--- app/routes/osint_scrapper.py
+++ app/routes/osint_scrapper.py
@@ -1,37 +1,36 @@
 from flask import (
-    Blueprint,
-    render_template,
-    session,
-    request,
-    redirect,
-    url_for,
+	Blueprint,
+	render_template,
+	session,
+	request,
+	redirect,
+	url_for,
 )
 from app.logic.osint_scrapper.osint_scrapper import osint_scrape
 
 osint_scrapper = Blueprint(
-    "osint_scrapper",
-    __name__,
-    template_folder="app/templates",
-    static_folder="app/static",
+	"osint_scrapper",
+	__name__,
+	template_folder="app/templates",
+	static_folder="app/static",
 )  # osint_scrapper: name of blueprint; __name__: import name of blueprint's module
 
 
 @osint_scrapper.route("/input", methods=["GET", "POST"])
 def input():
-    if request.method == "POST":
-        if request.form.get("Submit") == "Submit":
-            domain_name = request.form.get("domain_name")
-            option_list = request.form.getlist("option")
+	if request.method == "POST":
+		if request.form.get("Submit") == "START RECONNAISSANCE":
+			domain_name = request.form.get("domain_name")
+			option_list = request.form.getlist("option")
 
-            osint_result = osint_scrape(domain_name, option_list)
-            session["osint_option_list"] = option_list
-            session["osint_result"] = osint_result
-
-            return redirect(url_for("osint_scrapper.output"))
-    return render_template("osint_input.html")
+			osint_result = osint_scrape(domain_name, option_list)
+			session["osint_option_list"] = option_list
+			session["osint_result"] = osint_result
+			return redirect(url_for("osint_scrapper.output"))
+	return render_template("osint_input.html")
 
 
 @osint_scrapper.route("/output", methods=["GET"])
 def output():
-    osint_result = session["osint_result"]
-    return render_template("osint_output.html", osint_result=osint_result)
+	osint_result = session["osint_result"]
+	return render_template("osint_output.html", osint_result=osint_result)
--- app/routes/subdir_enumerator.py
+++ app/routes/subdir_enumerator.py
@@ -1,37 +1,41 @@
 from flask import (
-    Blueprint,
-    render_template,
-    session,
-    request,
-    redirect,
-    url_for,
+	Blueprint,
+	render_template,
+	session,
+	request,
+	redirect,
+	url_for,
 )
 
 from app.logic.subdir_enumerator.subdir_enumerator import subdir_enum
 
 
 subdir_enumerator = Blueprint(
-    "subdir_enumerator",
-    __name__,
-    template_folder="app/templates",
-    static_folder="app/static",
+	"subdir_enumerator",
+	__name__,
+	template_folder="app/templates",
+	static_folder="app/static",
 )
 
 @subdir_enumerator.route("/input", methods=["GET", "POST"])
 def input():
-    if request.method == "POST":
-        if request.form.get("Submit") == "Submit":
-            domain_name = request.form.get("domain_name")
-            
+	if request.method == "POST":
+		if request.form.get("Submit") == "START ENUMERATION":
+			# domain_name = request.form.get("domain_name") uncomment
+			# wordlist_size = request.form.get("wordlist_size") uncomment
 			# change to subdir function, and its parameters
-            subdir_result = subdir_enum(domain_name)
-            session["subdir_result"] = subdir_result
-
-            return redirect(url_for("subdir_enumerator.output"))
-    return render_template("subdir_input.html")
+			####
+			#test
+			domain_name = "google.com"
+			wordlist_size = "small".strip().lower()
+			subdir_result = subdir_enum(domain_name, wordlist_size)
+			session["subdir_result"] = subdir_result
+			print(subdir_result)
+			return redirect(url_for("subdir_enumerator.output"))
+	return render_template("subdir_input.html")
 
 
 @subdir_enumerator.route("/output", methods=["GET"])
 def output():
-    subdir_result = session["subdir_result"]
-    return render_template("subdir_output.html", subdir_result=subdir_result)
+	subdir_result = session["subdir_result"]
+	return render_template("subdir_output.html", subdir_result=subdir_result)
