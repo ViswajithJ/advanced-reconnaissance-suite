ef62637f053e84878435d636eff46e50cac8e924
Viswajith J <viswajith.j.official@gmail.com>
2025-03-19 00:17:40 +0530

    Add template for subdomain code

--- app/logic/subdom_enumerator/subdom_enumerator.py
+++ app/logic/subdom_enumerator/subdom_enumerator.py
@@ -1,2 +1,33 @@
-def subdom_enum(domain):
-	pass
+import os
+import asyncio
+
+
+counter = 1  # Global counter for numbering
+
+async def load_wordlist(wordlist_size):
+	try:
+		print(wordlist_size)
+		cwd=os.getcwd()
+		file_path = os.path.join(cwd, f"app/logic/subdom_enumerator/{wordlist_size}.txt")
+		print(file_path)
+		with open(file_path, 'r') as file:
+			return [line.strip() for line in file]
+	except FileNotFoundError:
+		return []
+
+##################################
+# Insert alan's code here
+##################################
+
+
+async def main(domain, wordlist_size):
+	result = await run_subdomain_scan(domain, wordlist_size, show_recursive=False)
+	return result
+
+
+
+def subdom_enum(domain, wordlist_size):
+	print(domain, wordlist_size)
+	result = asyncio.run(main(domain, wordlist_size))
+	print(result)
+	return result
--- app/routes/subdom_enumerator.py
+++ app/routes/subdom_enumerator.py
@@ -1,37 +1,38 @@
 from flask import (
-    Blueprint,
-    render_template,
-    session,
-    request,
-    redirect,
-    url_for,
+	Blueprint,
+	render_template,
+	session,
+	request,
+	redirect,
+	url_for,
 )
 
 from app.logic.subdom_enumerator.subdom_enumerator import subdom_enum
 
 
 subdom_enumerator = Blueprint(
-    "subdom_enumerator",
-    __name__,
-    template_folder="app/templates",
-    static_folder="app/static",
+	"subdom_enumerator",
+	__name__,
+	template_folder="app/templates",
+	static_folder="app/static",
 )
 
 @subdom_enumerator.route("/input", methods=["GET", "POST"])
 def input():
-    if request.method == "POST":
-        if request.form.get("Submit") == "Submit":
-            domain_name = request.form.get("domain_name")
-            
+	if request.method == "POST":
+		if request.form.get("Submit") == "START ENUMERATION":
+			domain_name = request.form.get("domain_name").strip().lower()
+			wordlist_size = request.form.get("wordlist_size").strip().lower()
+			
 			# change to subdom function, and its parameters
-            subdom_result = subdom_enum(domain_name)
-            session["subdom_result"] = subdom_result
-
-            return redirect(url_for("subdom_enumerator.output"))
-    return render_template("subdom_input.html")
+			subdom_result = subdom_enum(domain_name, wordlist_size)
+			session["subdom_result"] = subdom_result
+			print(subdom_result)
+			return redirect(url_for("subdom_enumerator.output"))
+	return render_template("subdom_input.html")
 
 
 @subdom_enumerator.route("/output", methods=["GET"])
 def output():
-    subdom_result = session["subdom_result"]
-    return render_template("subdom_output.html", subdom_result=subdom_result)
+	subdom_result = session["subdom_result"]
+	return render_template("subdom_output.html", subdom_result=subdom_result)
